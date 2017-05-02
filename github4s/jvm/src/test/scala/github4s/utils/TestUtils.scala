a183c1183458fc0ef9733cb38971cf1e0b5eaeba
adrian <adrianramirezfornell@gmail.com>
2017-05-02 12:21:18 +0200

    Create unit test util

--- github4s/jvm/src/test/scala/github4s/utils/TestUtils.scala
+++ github4s/jvm/src/test/scala/github4s/utils/TestUtils.scala
@@ -110,7 +110,20 @@ trait TestUtils {
 
   val validPullRequestFileSha = "f80f79cafbe3f2ba71311b82e1171e73bd37a470"
   val validPullRequestNumber  = 1
-  val validPath               = "project/plugins.sbt"
+
+  val validHead   = "pr-test"
+  val invalidHead = ""
+
+  val validBase   = "master"
+  val invalidBase = ""
+
+  val validNewPullRequestData   = NewPullRequestData("Amazing new feature", "Please pull this in!")
+  val invalidNewPullRequestData = NewPullRequestData("", "")
+
+  val validNewPullRequestIssue   = NewPullRequestIssue(26)
+  val invalidNewPullRequestIssue = NewPullRequestIssue(5)
+
+  val validPath = "project/plugins.sbt"
 
   val validStatusState = "success"
 
