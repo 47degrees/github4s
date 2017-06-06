a6d848de2c3de62457f24defd5610fe11c301656
Juan Pedro Moreno <juanpmorest@gmail.com>
2017-06-06 23:03:15 +0200

    Fixes cats-effect tests for scalajs

--- cats-effect/js/src/test/scala/github4s/cats/effect/js/CatsEffectJSSpec.scala
+++ cats-effect/js/src/test/scala/github4s/cats/effect/js/CatsEffectJSSpec.scala
@@ -20,32 +20,64 @@ import cats.effect.IO
 import github4s.Github
 import github4s.Github._
 import github4s.cats.effect.js.Implicits._
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{Assertion, AsyncFunSuite, Matchers}
 import fr.hmil.roshttp.response.SimpleHttpResponse
+import github4s.GithubResponses.GHResponse
+import github4s.free.domain.User
+import org.scalactic.source.Position
 
-class CatsEffectJSSpec extends FlatSpec with Matchers {
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future, Promise}
+import scala.util.Try
+
+class CatsEffectJSSpec extends AsyncFunSuite with Matchers {
+
+  implicit override def executionContext: ExecutionContextExecutor = ExecutionContext.global
+
+  def testEffectOnRunAsync[A](source: IO[GHResponse[A]], f: GHResponse[A] => Assertion)(
+      implicit pos: Position): Future[Assertion] = {
+
+    val effect  = Promise[GHResponse[A]]()
+    val attempt = Promise[Try[GHResponse[A]]]()
+    effect.future.onComplete(attempt.success)
+
+    val io = source.runAsync {
+      case Right(s) => IO(effect.success(s))
+      case Left(e)  => IO(effect.failure(e))
+    }
+
+    for (_ <- io.unsafeToFuture(); v <- attempt.future) yield {
+      v.toOption
+        .map { result =>
+          f(result)
+        }
+        .getOrElse(fail("effect attempt failed"))
+    }
+  }
+
   val accessToken     = sys.env.get("GITHUB4S_ACCESS_TOKEN")
   val headerUserAgent = Map("user-agent" -> "github4s")
   val validUsername   = "rafaparadela"
   val invalidUsername = "GHInvalidUserName"
   val okStatusCode    = 200
 
-  "cats-effect js integration" should "return a succeded result for a valid call" in {
+  test("return a succeded result for a valid call") {
     val response = Github(accessToken).users
       .get(validUsername)
       .exec[IO, SimpleHttpResponse](headerUserAgent)
 
-    val res = response.unsafeRunSync
-    res.isRight shouldBe true
-    res.right.map(_.statusCode) shouldBe Right(okStatusCode)
+    testEffectOnRunAsync(response, { r: GHResponse[User] =>
+      r.isRight shouldBe true
+      r.right.map(_.statusCode) shouldBe Right(okStatusCode)
+    })
   }
 
-  it should "return a failed result for an invalid call" in {
+  test("return a failed result for an invalid call") {
     val response = Github(accessToken).users
       .get(invalidUsername)
       .exec[IO, SimpleHttpResponse](headerUserAgent)
 
-    val res = response.unsafeRunSync
-    res.isLeft shouldBe true
+    testEffectOnRunAsync(response, { r: GHResponse[User] =>
+      r.isLeft shouldBe true
+    })
   }
 }
