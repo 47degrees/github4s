88550946ac19978c64425c340d9c4aada742b06f
Javier de Siloniz Sandino <jsandino@starbuck.com>
2019-07-16 08:58:49 -0700

    Testing if the scalac options are needed anymore after the stack size upgrade

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -124,10 +124,6 @@ object ProjectPlugin extends AutoPlugin {
       scalaVersion := V.scala212,
       crossScalaVersions := Seq(V.scala211, V.scala212),
       scalacOptions ~= (_ filterNot Set("-Xlint").contains),
-      scalacOptions ++= ((scalaBinaryVersion.value, getEnvVar("SCALAENV")) match {
-        case (x, env) if x.startsWith("2.12") && !env.contains("js") => Seq("-Ywarn-macros:after")
-        case _                         => Seq.empty
-      }),
       orgGithubTokenSetting := "GITHUB4S_ACCESS_TOKEN",
       resolvers += Resolver.bintrayRepo("hmil", "maven"),
       orgBadgeListSetting := List(
