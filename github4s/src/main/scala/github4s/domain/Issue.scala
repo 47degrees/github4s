297681a84ac99e5367ec9822366cb70b139be3a7
Ana Marquez Velazquez <ana@Anas-MacBook-Pro-2.local>
2020-03-02 16:56:54 +0100

    merge with master

--- github4s/src/main/scala/github4s/domain/Issue.scala
+++ github4s/src/main/scala/github4s/domain/Issue.scala
@@ -103,25 +103,3 @@ case class Milestone(
     closed_at: Option[String],
     due_on: String
 )
-
-//TODO: Remove when merge with branch 354 List project)
-final case class Creator(
-    login: String,
-    id: Int,
-    node_id: String,
-    avatar_url: String,
-    gravatar_id: Option[String],
-    url: String,
-    html_url: String,
-    followers_url: String,
-    following_url: String,
-    gists_url: String,
-    starred_url: String,
-    subscriptions_url: String,
-    organizations_url: String,
-    repos_url: String,
-    events_url: String,
-    received_events_url: String,
-    `type`: String,
-    site_admin: Boolean
-)
