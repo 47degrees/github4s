d6b01769298dfc531d3d3cfa53598a37efe07db4
James Santucci <james.santucci@47deg.com>
2022-01-21 15:43:36 -0600

    scalafmt

--- github4s/jvm/src/test/scala/github4s/utils/TestContext.scala
+++ github4s/jvm/src/test/scala/github4s/utils/TestContext.scala
@@ -4,7 +4,7 @@ import scala.concurrent.ExecutionContext
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object TestContext {
-    // See TestContext in JS test code for an explanation of why this value
-    // is living off in an object
-    val context: ExecutionContext = global
+  // See TestContext in JS test code for an explanation of why this value
+  // is living off in an object
+  val context: ExecutionContext = global
 }
