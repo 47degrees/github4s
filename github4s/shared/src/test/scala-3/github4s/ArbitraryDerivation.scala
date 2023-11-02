463415a37fac424663dcd16f17536fdb738cc646
Fede Fernández <720923+fedefernandez@users.noreply.github.com>
2023-11-02 09:55:48 +0100

    bump plugins

--- github4s/shared/src/test/scala-3/github4s/ArbitraryDerivation.scala
+++ github4s/shared/src/test/scala-3/github4s/ArbitraryDerivation.scala
@@ -9,7 +9,6 @@ object ArbitraryDerivation { self =>
   object semiauto:
     extension (A: Arbitrary.type)
       inline def derived[A]: Arbitrary[A] =
-        import auto.given
         summonInline[Arbitrary[A]]
 
   inline def deriveArb[A](using g: K0.Generic[A]): Arbitrary[A] =
@@ -26,20 +25,24 @@ object ArbitraryDerivation { self =>
 // https://scastie.scala-lang.org/b8CXWfmXQUClqXplyA6TJg
 opaque type DerivedGen[A] = Gen[A]
 object DerivedGen extends DerivedGenInstances:
-  given [A]: Conversion[Gen[A], DerivedGen[A]] = identity
+  given [A]: Conversion[Gen[A], DerivedGen[A]]            = identity
   extension [A](gen: DerivedGen[A]) def arb: Arbitrary[A] = Arbitrary(gen)
 
   private final class ArrayProduct(elems: Array[Any]) extends Product:
-    def canEqual(that: Any): Boolean = true
-    def productElement(n: Int): Any = elems(n)
-    def productArity: Int = elems.length
+    def canEqual(that: Any): Boolean            = true
+    def productElement(n: Int): Any             = elems(n)
+    def productArity: Int                       = elems.length
     override def productIterator: Iterator[Any] = elems.iterator
 
   def product[A](arbs: Seq[Arbitrary[_]])(using gen: K0.ProductGeneric[A]): DerivedGen[A] =
-    Gen.sequence[Array[Any], Any](arbs.map(_.arbitrary)).map(arr => gen.fromProduct(ArrayProduct(arr)).asInstanceOf[A])
+    Gen
+      .sequence[Array[Any], Any](arbs.map(_.arbitrary))
+      .map(arr => gen.fromProduct(ArrayProduct(arr)).asInstanceOf[A])
 
   def coproduct[A](arbs: => IndexedSeq[Arbitrary[_ <: A]]): DerivedGen[A] =
-    Gen.lzy(if arbs.isEmpty then Gen.fail else Gen.choose(0, arbs.length - 1).flatMap(arbs(_).arbitrary))
+    Gen.lzy(
+      if arbs.isEmpty then Gen.fail else Gen.choose(0, arbs.length - 1).flatMap(arbs(_).arbitrary)
+    )
 
 sealed abstract class DerivedGenInstances:
   inline def summonAllArb[A, U](using gen: K0.Generic[A]): IndexedSeq[Arbitrary[_ <: U]] =
